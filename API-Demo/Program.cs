a92c0eaf74ee764244f59eb258e08e1b8241bdbe
andrew <aheyworth@live.com>
2023-03-17 19:12:22 +0000

    Added Litedb

--- API-Demo/Program.cs
+++ API-Demo/Program.cs
@@ -1,5 +1,6 @@
 using Library;
 using Microsoft.AspNetCore.OpenApi;
+using LiteDB;
 
 namespace API_Demo;
 
@@ -8,6 +9,13 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        //you could also get it from IConfiguration interface
+        var connectionString = "temp.db";
+
+        //add as a singleton - it's a single file with a single access point
+        builder.Services.AddSingleton<ILiteDatabase, LiteDatabase>(
+        x => new LiteDatabase(connectionString));
 
         // Add services to the container.
         builder.Services.AddAuthorization();
@@ -29,19 +37,26 @@ public class Program
 
         app.UseAuthorization();
 
-        app.MapGet("/todo", (HttpContext httpContext) =>
+        app.MapGet("/todo", () =>
         {
-            List<TodoItem> list = new List<TodoItem>() { 
+            List<TodoItem> list = new List<TodoItem>() {
                 new TodoItem() { Id = 1, Text = "This is a list of text.", IsComplete = true } ,
                 new TodoItem() { Id = 2, Text = "New todo item.", IsComplete = false } ,
                 new TodoItem() { Id = 3, Text = "Make toast.", IsComplete = false } ,
                 new TodoItem() { Id = 4, Text = "Collect stuff.", IsComplete = true } ,
-                new TodoItem() { Id = 5, Text = "Manage things.", IsComplete = true } 
+                new TodoItem() { Id = 5, Text = "Manage things.", IsComplete = true }
                 };
-            return list;
-
+            return list == null ? Results.NotFound() : Results.Ok(list);
         })
         .WithName("GetTodo")
+        .WithOpenApi();
+
+        app.MapGet("/items", (ILiteDatabase db) =>
+        {
+            var items = db.GetCollection<TodoItem>("TodoItems").FindAll().ToList();
+            return items == null ? Results.NotFound() : Results.Ok(items);
+        })
+        .WithName("GetItems")
         .WithOpenApi();
 
         app.Run();
